94e1d291faf00ade914e058f78a15aa29ce3585a
blair drummond <blairdrummond@protonmail.com>
2020-05-05 16:43:09 +0000

    feat: add python self-serve minio example

---
+++ self-serve-storage/python/daaas_storage.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import subprocess
+from minio import Minio
+import os
+
+def __get_minio_client__(tenant):
+    """ Get the variables out of vault to create Minio Client. """
+
+    d = {
+        'MINIO_URL'        : None,
+        'MINIO_ACCESS_KEY' : None,
+        'MINIO_SECRET_KEY' : None
+    }
+
+    if tenant not in ("minimal", "premium", "pachyderm"):
+        print("Not a valid resource! Options are")
+        print("minimal, premium, pachyderm.")
+        print("We will try anyway...")
+
+    vault = f"/vault/secrets/minio-{tenant}-tenant1"
+
+    for var in d: 
+        CMD = f'echo $(source {vault}; echo ${var})'
+        p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True,
+                             executable='/bin/bash')
+        d[var] = p.stdout.readlines()[0].strip().decode("utf-8")
+
+    import re
+    # Get rid of http:// in minio URL
+    http = re.compile('^https?://')
+        
+    # Create the minio client.
+    s3Client = Minio(
+        http.sub("", d['MINIO_URL']),
+        access_key=d['MINIO_ACCESS_KEY'],
+        secret_key=d['MINIO_SECRET_KEY'],
+        secure=False,
+        region="us-west-1"
+    )
+
+    return s3Client
+
+
+
+def get_minimal_client():
+    """Get a connection to the minimal Minio tenant"""
+    return __get_minio_client__("minimal")
+
+def get_pachyderm_client():    
+    """Get a connection to the pachyderm Minio tenant"""
+    return __get_minio_client__("pachyderm")
+
+def get_premium_client():
+    """Get a connection to the premium Minio tenant"""
+    return __get_minio_client__("premium")
