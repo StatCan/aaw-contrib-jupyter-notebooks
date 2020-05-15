fbd919cb1a85f5ab2071586d978cc70680451933
Andrew Scribner <ca.scribner+1@gmail.com>
2020-05-15 19:40:12 +0000

    Refactor map-reduce example's average.sh inputs

--- mapreduce-pipeline/average/average.sh
+++ mapreduce-pipeline/average/average.sh
@@ -8,8 +8,10 @@
 # Take an average to estimate pi.
 #
 # Input:
-#     --numbers  Sequence of JSON strings that each include the field "result"
+# (one of numbers and input_path must be specified)
 #     --output   (optional) Output bucket/folder
+#     --numbers  Sequence of JSON strings that each include the field "result"
+#     --input_paths   Sequence of locations in minio to load results files from
 #
 # Output:
 #     ./output/out.json
@@ -21,12 +23,27 @@ HOST_ALIAS="ARIBTRARY-HOST-ALIAS-FOR-MINIO-MOUNT"
 OUTPUT_LOCAL_DIR=./output
 LOCAL_INPUT_STORAGE=./input.txt
 
+
 # Remove input/output swap files, just in case (only really needed for local development)
+rm -r $OUTPUT_LOCAL_DIR
 rm $LOCAL_INPUT_STORAGE
-rm -rf $OUTPUT_LOCAL_DIR
+
+
+# Helper function to connect to minio host (if not already connected)
+connect_minio()
+{
+    if [ -z $MINIO_CONNECTED ]; then
+      echo mc config host add $HOST_ALIAS \
+            "$MINIO_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+        mc config host add $HOST_ALIAS \
+            "$MINIO_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+        MINIO_CONNECTED=1
+    fi
+}
 
 
 # Parse inputs
+MINIO_PATHS=()
 while test -n "$1"; do
     case "$1" in
         --output)
@@ -34,18 +51,39 @@ while test -n "$1"; do
             OUTPUT="$1"
             ;;
 
-        --numbers) ;;
-        # push .result from JSON numbers to a file for later use
-            
-        *)
+        --json) 
+            # store result from json in file
+            shift
             printf '%d\n' $(jq -nr "$1 | .result") >> $LOCAL_INPUT_STORAGE
+
+            ;;
+
+        --minio_path) 
+            # push minio url's to file for later use
+            shift
+            MINIO_PATHS+=($1)
+            ;;
+
+        *)
+            echo "Invalid option $1; allowed: --params --options" >&2
+            exit 1
             ;;
     esac
     shift
 done
 
 
-# Check for required data
+# For any input specified by minio, collect it and push it to the same 
+# local input storage as for json
+for mpath in ${MINIO_PATHS[@]}; 
+do
+    connect_minio
+    this_result=`mc cat "$HOST_ALIAS/$mpath" | jq .result`
+    printf '%d\n' $this_result >> $LOCAL_INPUT_STORAGE
+done
+
+
+# Check that we have at least SOME data
 if [ ! -s $LOCAL_INPUT_STORAGE ] 
 then
     echo "Error: Found no inputs numbers for averaging"
@@ -57,7 +95,7 @@ fi
 mkdir -p $OUTPUT_LOCAL_DIR
 
 
-# Do the estimate of Pi
+# Do the estimate of Pi, saved to out.json
 awk '{sum+=$1} END {
     pi = 4*sum/NR;
     printf("{ \"pi\" : %.12f, \"samples\" : %d }\n", pi, NR);
@@ -72,11 +110,8 @@ then
     OUTPUT=`echo $OUTPUT | sed 's/\/*$/\//g'`
 
     echo "Push results to MINIO_URL: ${MINIO_URL}"
-    echo mc config host add $HOST_ALIAS \
-        "$MINIO_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
+    connect_minio
 
-    mc config host add $HOST_ALIAS \
-        "$MINIO_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
 
     echo copying to minio storage with command:
     echo mc cp $OUTPUT_LOCAL_DIR/* "$HOST_ALIAS/$OUTPUT"
