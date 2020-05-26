a36eeade699819ece6a2ba0c85de6227e65480bd
Andrew Scribner <ca.scribner+1@gmail.com>
2020-05-26 15:55:42 -0400

    Simplify average.sh and sample.sh.  Remove all minio references

--- mapreduce-pipeline/average/average.sh
+++ mapreduce-pipeline/average/average.sh
@@ -1,84 +1,42 @@
 #!/usr/bin/env bash
 
-# Computes 4*average of one or more numbers provided as JSON input arguments
+# Computes 4*average of one or more numbers provided as JSON strings
 # 
 # Intended use is for computing an estimate of pi by using in conjunction with 
 # with sample.sh.
-
+# 
 # Take an average to estimate pi.
 #
 # Input:
-# (one of numbers and input_path must be specified)
-#     --output   (optional) Output bucket/folder
 #     --numbers  Sequence of JSON strings that each include the field "result"
-#     --input_paths   Sequence of locations in minio to load results files from
 #
 # Output:
 #     ./output/out.json
 #         JSON of pi (4*average of inputs) and samples (n_inputs)
 
+echo "Received command line args:"
+echo $@
+echo "---------------------------"
 
 # Settings (probably don't need to change these)
-HOST_ALIAS="ARIBTRARY-HOST-ALIAS-FOR-MINIO-MOUNT"
 OUTPUT_LOCAL_DIR=./output
 LOCAL_INPUT_STORAGE=./input.txt
-MINIO_URL="http://$MINIO_URL"
 
 # Remove input/output swap files, just in case (only really needed for local development)
 rm -r $OUTPUT_LOCAL_DIR
 rm $LOCAL_INPUT_STORAGE
 
-
-# Helper function to connect to minio host (if not already connected)
-connect_minio()
-{
-    if [ -z $MINIO_CONNECTED ]; then
-      echo mc config host add $HOST_ALIAS \
-            "$MINIO_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
-        mc config host add $HOST_ALIAS \
-            "$MINIO_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
-        if [ $? -ne 0 ]
-        then
-            echo "Failed to push all files to minio.  Check above errors from minio"
-            exit 1
-        fi
-        MINIO_CONNECTED=1
-
-    fi
-}
-
-
-echo "Received command line args:"
-echo $@
-echo "---------------------------"
-
-
 # Parse inputs
-
-MINIO_PATHS=()
-
 while test -n "$1"; do
     case "$1" in
-        --output)
-            shift
-            OUTPUT="$1"
-            ;;
-
         --json) 
             # store result from json in file
             shift
             printf '%d\n' $(jq -nr "$1 | .result") >> $LOCAL_INPUT_STORAGE
-
-            ;;
-
-        --minio_path) 
-            # push minio url's to file for later use
-            shift
-            MINIO_PATHS+=($1)
             ;;
 
         *)
-            echo "Invalid option $1; allowed: --params --options" >&2
+            echo "Invalid option $1; allowed: --json" >&2
             exit 1
             ;;
     esac
@@ -86,63 +44,20 @@ while test -n "$1"; do
 done
 
 
-# For any input specified by minio, collect it and push it to the same 
-# local input storage as for json
-for mpath in ${MINIO_PATHS[@]}; 
-do
-    connect_minio
-
-    # Not sure how to make the printf below show a failure code if mc fails, so test first then get
-    mc ls "$HOST_ALIAS/$mpath"
-    if [ $? -ne 0 ]
-    then
-        echo "Failed to push all files to minio.  Check above errors from minio"
-        exit 1
-    fi
-    printf '%d\n' `mc cat "$HOST_ALIAS/$mpath" | jq .result` >> $LOCAL_INPUT_STORAGE
-
-done
-
-
-# Check that we have at least SOME data
+# Validate input
 if [ ! -s $LOCAL_INPUT_STORAGE ] 
 then
     echo "Error: Found no inputs numbers for averaging"
     exit 1
 fi
 
-
 # Setup workspace
 mkdir -p $OUTPUT_LOCAL_DIR
 
-
-# Do the estimate of Pi, saved to out.json
+# Estimate pi and save to out.json
 awk '{sum+=$1} END {
     pi = 4*sum/NR;
     printf("{ \"pi\" : %.12f, \"samples\" : %d }\n", pi, NR);
 }' $LOCAL_INPUT_STORAGE | jq '.' | tee $OUTPUT_LOCAL_DIR/out.json
-
-
-# If required, push results to Minio
-if [ -n "${OUTPUT}" ]
-then
-    # Ensure OUTPUT ends in a single /.  Minio doesn't like if there's multiple /'s
-    # and we need at least one as this is a minio "directory"
-    OUTPUT=`echo $OUTPUT | sed 's/\/*$/\//g'`
-
-    echo "Push results to MINIO_URL: ${MINIO_URL}"
-    connect_minio
-
-
-    echo copying to minio storage with command:
-    echo mc cp $OUTPUT_LOCAL_DIR/* "$HOST_ALIAS/$OUTPUT"
-    mc cp $OUTPUT_LOCAL_DIR/* "$HOST_ALIAS/$OUTPUT"
-    if [ $? -ne 0 ]
-    then
-        echo "Failed to push all files to minio.  Check above errors from minio"
-        exit 1
-    fi
-fi
-
 
 echo "Done."
--- mapreduce-pipeline/sample/sample.sh
+++ mapreduce-pipeline/sample/sample.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Randomly picks an (x,y) point inside a 2x2 square then returns 
+# Randomly picks an (x,y) point inside a 2x2 square returns 
 # whether that point resides inside a unit circle (circle with r=1)
 # centered in the square in an output file
 #
@@ -12,69 +12,43 @@
 #
 # Input:
 #     --params  Json string containing integer "seed"
-#     --output  (optional) Output bucket/folder
-#     --sleep   (optional) Sleep for this many seconds before working.  
-#               Useful to make a longer process to highlight whether the
-#               average step is in a race condition with sample.
-#
+# 
 # Output:
 #     ./output/in.json
 #         JSON of input parameters (received from --params)
 #     ./output/out.json
 #         JSON of (x, y) coordinates tested and "result" (0==outside, 
 #         1==inside circle)
-#     (note: local ./output location can be changed via OUTPUT_LOCAL_DIR)
-#     If --output arg specified, output is also pushed to minio storage
-#     at that location.  Minio credentials are defined via environment 
-#     variables MINIO_URL, MINIO_ACCESS_KEY, and MINIO_SECRET_KEY
-
-
-# Settings (probably don't need to change these)
-HOST_ALIAS="ARIBTRARY-HOST-ALIAS-FOR-MINIO-MOUNT"
-OUTPUT_LOCAL_DIR=./output
-MINIO_URL="http://$MINIO_URL"
-
+#     (note: local ./output location can be changed by editing
+#     OUTPUT_LOCAL_DIR)
 
 echo "Received command line args:"
 echo $@
 echo "---------------------------"
 
+# Settings (probably don't need to change these)
+OUTPUT_LOCAL_DIR=./output
+
+# Remove previous output, just in case (only really needed for local development)
+rm -r $OUTPUT_LOCAL_DIR
 
 # Parse input arguments
 while test -n "$1"; do
     case "$1" in
-        --output)
-            shift
-            OUTPUT="$1"
-            ;;
-
         --params)
             shift
             JSON="$1"
             ;;
 
-        --sleep)
-            shift
-            echo "Sleeping for ${1}s"
-            sleep $1
-            ;;
         *)
-            echo "Invalid option $1; allowed: --params --options" >&2
+            echo "Invalid option $1; allowed: --params" >&2
             exit 1
             ;;
     esac
     shift
 done
 
-# Check for required parameter params
-if [ -z "$JSON" ] 
-then
-    echo "Missing required argument --params"
-    exit 1
-fi
-
-
-# Log input and output
+# Log input and create output location
 mkdir -p $OUTPUT_LOCAL_DIR
 echo "Input: "
 echo "$JSON" | jq '.' | tee $OUTPUT_LOCAL_DIR/in.json
@@ -84,11 +58,11 @@ echo "Output: "
 SEED=$(jq -rn "$JSON | .seed")
 if [ $? -ne 0 ]
 then
-    echo "Invalid input to --params.  Must be a json string with key seed"
+    echo "Invalid input to --params.  Must be a json string with key of seed"
     exit 1
 fi
 
-
+# Put a point in a circle!
 awk -v seed=$SEED 'BEGIN {
        srand(seed);
        x = rand() * 2 - 1;
@@ -97,38 +71,5 @@ awk -v seed=$SEED 'BEGIN {
        printf("{ \"x\" : %.3f, \"y\" : %.3f, \"result\" : %d }\n", x, y, result);
 }' | tee $OUTPUT_LOCAL_DIR/out.json
 echo "Result written to $OUTPUT_LOCAL_DIR/out.json"
-
-echo "Minio OUTPUT before processing = '$OUTPUT'"
-
-# If required, push results to Minio
-if [ -n "${OUTPUT}" ]
-then
-    # Ensure OUTPUT ends in a single /.  Minio doesn't like if there's multiple /'s
-    # and we need at least one as this is a minio "directory"
-    OUTPUT=`echo $OUTPUT | sed 's/\/*$/\//g'`
-
-    echo "Push results to MINIO_URL: ${MINIO_URL}"
-    echo mc config host add $HOST_ALIAS \
-        "$MINIO_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
-
-    mc config host add $HOST_ALIAS \
-        "$MINIO_URL" "$MINIO_ACCESS_KEY" "$MINIO_SECRET_KEY"
-    if [ $? -ne 0 ]
-    then
-        echo "Failed to push all files to minio.  Check above errors from minio"
-        exit 1
-    fi
-
-    echo copying to minio storage with command:
-    echo mc cp $OUTPUT_LOCAL_DIR/* "$HOST_ALIAS/$OUTPUT"
-    mc cp $OUTPUT_LOCAL_DIR/* "$HOST_ALIAS/$OUTPUT"
-    # Test for success
-    if [ $? -ne 0 ]
-    then
-        echo "Failed to push all files to minio.  Check above errors from minio"
-        exit 1
-    fi
-fi
-
 
 echo "Done."
