77dee2ea4f69d9a05cf7d5817822f3783e257080
Salwa <51963397+jumana-s@users.noreply.github.com>
2021-12-15 12:18:51 -0500

    Update daaas_storage.py

--- self-serve-storage/python/daaas_storage.py
+++ self-serve-storage/python/daaas_storage.py
@@ -14,8 +14,8 @@
 ###      API     ###
 ####################
 ###
-###  minio_client =  get_standard_client()      # return a Minio object
-###  minio_client =  get_premium_client()      # return a Minio object
+###  instances =  get_instances()      # return all available MinIO instances
+###  minio_client =  get_minio_client(instance)      # return a Minio object, pass in a valid MinIO instance
 ###
 ###  See: https://github.com/minio/minio-py
 
@@ -25,9 +25,8 @@
 ###
 ###    import daaas_storage.py
 ###
-###    # Choose from
-###    minio_client = get_standard_client():
-###    # minio_client = get_premium_client():
+###    instances = get_instances()
+###    minio_client = get_minio_client(instances[0])    # pick an instance
 ###
 ###
 ###    # This minio client is from minio-py, and you can use it
@@ -53,17 +52,10 @@ from minio import Minio
 import os
 import json
 
-def __get_minio_client__(tenant):
+def get_client(instance):
     """ Get the variables out of vault to create Minio Client. """
 
-    if tenant not in ("standard", "premium"):
-        print("Not a valid resource! Options are")
-        print("standard, premium")
-        print("We will try anyway...")
-
-    #vault = f"/vault/secrets/minio-{tenant}-tenant-1"
-
-    with open(f'/vault/secrets/minio-{tenant}-tenant-1.json') as f:
+    with open(f'/vault/secrets/{instance}.json') as f:
         creds = json.load(f)
         minio_url = creds['MINIO_URL']
 
@@ -82,11 +74,22 @@ def __get_minio_client__(tenant):
 
     return s3Client
 
+class Instances:
+    instances = []
+    def add_instance(self, instance):
+        self.instances.append(instance)
+        setattr(self, instance, get_client(instance)) 
+    
+    def __init__(self):
+        instances = get_instances()
+        for i in instances:
+            self.add_instance(i)
+            
+def get_instances():
+    files = os.listdir('/vault/secrets')
+    instances = [file for file in files if not file.endswith('.json')]
+    return instances
 
-def get_standard_client():
-    """Get a connection to the minimal Minio tenant"""
-    return __get_minio_client__("standard")
+# instances = Instances() 
 
-def get_premium_client():
-    """Get a connection to the premium Minio tenant"""
-    return __get_minio_client__("premium")
+
